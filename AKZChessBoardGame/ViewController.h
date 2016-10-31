0bd5976370803d88fc59994b82c9cc4329dda59a
sphatzik <sphatzik@gmail.com>
2016-10-31 11:09:39 +0200

    Initial Commit

---
+++ AKZChessBoardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AKZChessBoardGame
+//
+//  Created by minus one on 31/10/16.
+//  Copyright © 2016 Spyridon Chatzikotoulas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
